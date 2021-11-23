2f53a7bb061980496f58d6016c47cabc6abb21ee
Javier Bolaños Carapia <javier.bc121086@gmail.com>
2021-11-22 23:27:15 -0600

    Initial Commit

---
+++ ghgungnircore/ghgungnircore.h
@@ -0,0 +1,18 @@
+//
+//  ghgungnircore.h
+//  ghgungnircore
+//
+//  Created by Javier Carapia on 22/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ghgungnircore.
+FOUNDATION_EXPORT double ghgungnircoreVersionNumber;
+
+//! Project version string for ghgungnircore.
+FOUNDATION_EXPORT const unsigned char ghgungnircoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ghgungnircore/PublicHeader.h>
+
+
